453b29dacd0aacf05d723cb6e84de0990f8de24b
Fernando May <fmay130@gmail.com>
2022-08-24 17:41:02 -0500

    Initial Commit

---
+++ Shared/dessertsApp.swift
@@ -0,0 +1,17 @@
+//
+//  dessertsApp.swift
+//  Shared
+//
+//  Created by Fernando May on 24/08/22.
+//
+
+import SwiftUI
+
+@main
+struct dessertsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
